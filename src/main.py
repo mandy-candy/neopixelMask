087a3e4835f6524ec5899e9703da26c4f72ba793
Amanda Kuenzle <amandakunzle@gmail.com>
2021-01-13 21:15:17 +0100

    neopixels working

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 # main.py
+
+from machine import Pin
+import neopixel
+
+# one ring has 16 pixels
+pixel = neopixel.NeoPixel(Pin(5), 32)
+# get number of pixels out of object
+index = pixel.n
+
+
+for i in range(index):
+    pixel[i] = (10, 0, 0)
+    pixel.write()  
